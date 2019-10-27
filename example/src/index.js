5e862cebeb5b077c0a5687fdf17ebd4658960313
Spice-Z <yugo.ogura@eure.jp>
2019-10-27 23:17:37 +0900

    create-react-app でひな形を作成

---
+++ example/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+  return (
+    <div>init</div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
